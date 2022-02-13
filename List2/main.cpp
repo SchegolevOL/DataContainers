a9a20176fdd860ecb695d2d30ebe6878d6ccd0b3
SchegolevOL <92328776+SchegolevOL@users.noreply.github.com>
2022-02-13 20:52:18 +0500

    start

---
+++ List2/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <Windows.h>
+
+#define tab "\t"
+
+using namespace std;
+
+
+
+
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+
+
+
+
+	return 0;
+}
